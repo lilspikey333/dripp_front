7d0d296714b42cb6781c0de07988cca22dfb272d
Jen Valladares <lilspikey333@yahoo.com>
2020-02-12 12:19:11 -0600

    login form component created

--- dripp/src/App.js
+++ dripp/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- dripp/src/components/Home/index.js
+++ dripp/src/components/Home/index.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function Home(props) {
   return (
-    <div>
-        This is the homepage
+    <div class="home">
+      <form action="action_page.php" method="post">
+        <label>Username:</label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
