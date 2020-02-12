4a441ee81e8a57bb91e7fc54e74aa3a89db43280
Jen Valladares <lilspikey333@yahoo.com>
2020-02-12 11:45:46 -0600

    initial front-end create

---
+++ dripp/src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./footer.css";
+
+function Footer(props) {
+    return (
+        <footer class="footer">Thank you for using Dripp for all of your clothing needs!</footer>
+    )
+}
+export default Footer
---
+++ dripp/src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Home(props) {
+    return (
+        <section class="login=form">
+            <form>
+                <input ></input>
+            </form>
+        </section>
+    )
+}
+export default Home
